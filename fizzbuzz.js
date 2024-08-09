e00e9b871b01fcb0fce9b46d4d154c1b3391740c
Vishnu <96681356+VishnuSai2000@users.noreply.github.com>
2024-08-09 15:57:46 +0530

    Update fizzbuzz.js

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,8 +1,10 @@
-let output = fizzBuzz(5);
+const num =prompt("Enter the Number");
+let output = fizzBuzz(Number(num));
 console.log(output);
 
 function fizzBuzz(input){
-    if  (typeof input !== 'number')
+    //if(isNaN(input)) --- this is the alternate form of the below if condition. 
+    if  (typeof input !== 'number')   
         return 'Not a Number';
     
     else if (input%3 == 0 && input%5 == 0)
